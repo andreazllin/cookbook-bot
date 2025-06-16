45069df3d12543bc32d40286d585b0c6e5adc5d2
Andrea Lin <me@andrealin.it>
2025-06-17 00:20:41 +0200

    feat: example commands

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import type { Env } from "@/types"
+import { Bot, webhookCallback } from "grammy"
 import { Hono } from "hono"
 
 const app = new Hono<{ Bindings: Env }>()
 
 app.get("/", (c) => {
   return c.text("Hello Hono!")
+})
+
+app.post("/webhook", async (c) => {
+  const bot = new Bot(c.env.BOT_TOKEN)
+
+  bot.command("start", (ctx) => {
+    return ctx.reply("Welcome to the bot!")
+  })
+
+  const handler = webhookCallback(bot, "hono")
+  return await handler(c)
 })
 
 export default app
